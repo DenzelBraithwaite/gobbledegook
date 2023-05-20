91b54d52e8875a22d86f7351b43ecd3c28b9dcd0
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-05-20 15:50:44 -0400

    removing some default svelte template stuff

---
+++ src/lib/Card.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let count: number = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<div class="card">
+  <p>I'm a playing card</p>
+</div>
+
+<style>
+  .card {
+    width: 125px;
+    height: 150px;
+    position: relative;
+    text-align: center;
+    padding: 1rem;
+    background-color: #fff;
+    border-radius: 0.25rem;
+    margin: 0 auto;
+  }
+</style>
