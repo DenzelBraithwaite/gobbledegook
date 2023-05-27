f10459c28a8af476bd6e151c8c51298bcf116dd4
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-05-27 10:32:46 -0400

    added svelte-routing library as a dependency

---
+++ src/routes/home.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    // GobbledeGook card
+    import GGCard from '../lib/Card.svelte';
+    import Api from '../lib/Api.svelte';
+  
+    // Other custom components
+    import LoginForm from '../lib/LoginForm.svelte';
+  
+    import {Link} from 'svelte-routing';
+    import Home from '../routes/home.svelte';
+  
+  </script>
+  
+  <main>
+    <LoginForm />
+    <div class="container">
+      <!-- <GGCard /> -->
+      <Api />
+      <Link to='/'>click me to go home</Link>
+    </div>
+  </main>
+  
+  <style>
+    main {
+      padding-top: 5rem;
+      position: relative;
+      background-image: url('/gg.png');
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center;
+    }
+    
+    .container {
+      padding-top: 1rem;
+      background-color: #633f1e;
+      width: 60%;
+      max-width: 1000px;
+      margin: 0 auto;
+      min-height: 100vh;
+      filter: blur(0px);
+    }
+  </style>
+  
---
+++ src/routes/login.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import LoginForm from '../lib/LoginForm.svelte';
+  
+</script>
+  
+  <main>
+    <div class="login-container">
+        <LoginForm />
+    </div>
+  </main>
+  
+<style>
+    main {
+        height: 100vh;
+        width: 100vw;
+        position: relative;
+        background-image: linear-gradient(to top, #000000bb, transparent), url('/gg.png');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+  
