c06b5f82d2d007c23ef108be37c29dc030a88760
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2024-02-03 15:57:03 -0500

    added index for components for easier import

---
+++ src/lib/index.ts
@@ -0,0 +1,13 @@
+import Button from './Button.svelte';
+import Card from './Card.svelte';
+// import Chat from './Chat.svelte';
+import Game from './Game.svelte';
+// import LoginForm from './LoginForm.svelte';
+
+export {
+  Button,
+  Card,
+  // Chat,
+  Game,
+  // LoginForm
+}
